15e212a58b5887ddbb98543e620d8528992bd341
bstepic <bstepic@emisia.net>
2019-04-02 15:10:18 +0200

    spelling fix

--- src/components/home/partials/NothingFound.js
+++ src/components/home/partials/NothingFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NothingFound() {
     return (
         <div className="nothing-found">
-            No results found for following term, please try again with different term.
+            No results found for following term, please try again with a different term.
         </div>
     );
 }
