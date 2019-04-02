5beeda190adeb1ab44278e7e418ead9c74c7d75b
bstepic <bstepic@emisia.net>
2019-04-02 14:41:16 +0200

    added google fonts, open review in new tab

--- src/components/home/partials/ResultItem.js
+++ src/components/home/partials/ResultItem.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {Card, CardBody, CardLink, CardText, CardTitle} from "reactstrap";
 
-export default function ResultItem(props) {
+export default function ResultItem(props){
     const {data, type} = props;
     return (
         <Card className="result-item shadow-sm">
             <CardBody>
-                <CardTitle>Review by {data.byline}, <small className="text-muted">published on {data.publication_dt}</small></CardTitle>
+                <CardTitle>Review by {data.byline},
+                    <small className="text-muted">
+                        published on {data.publication_dt}
+                    </small>
+                </CardTitle>
                 <CardText>
                     {type === 'author' ? "Title " : "Authored by "}
                     <strong>
                         {type === 'author' ? data.book_title : data.book_author}
                     </strong>
                 </CardText>
-                <CardLink href={data.url}>go to review...</CardLink>
+                <CardLink href={data.url} target="_blank">go to review...</CardLink>
             </CardBody>
         </Card>
     );
