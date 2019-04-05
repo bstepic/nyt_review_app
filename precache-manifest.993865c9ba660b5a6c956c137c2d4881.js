2a76623e1d671f8a57b7bbe543ca9049eb435331
bstepic <bstepic@emisia.net>
2019-04-05 12:02:24 +0200

    Updates

---
+++ precache-manifest.993865c9ba660b5a6c956c137c2d4881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c97a9634c64bfe6a85f9",
+    "url": "/nyt_review_app/static/js/runtime~main.4d3804c6.js"
+  },
+  {
+    "revision": "70492bf76421b0706be0",
+    "url": "/nyt_review_app/static/js/main.1e77d937.chunk.js"
+  },
+  {
+    "revision": "acd14102dbb6516071bf",
+    "url": "/nyt_review_app/static/js/2.5a743396.chunk.js"
+  },
+  {
+    "revision": "70492bf76421b0706be0",
+    "url": "/nyt_review_app/static/css/main.6c93e4f0.chunk.css"
+  },
+  {
+    "revision": "118f0424d8bac2c8afff7f10371c79e2",
+    "url": "/nyt_review_app/index.html"
+  }
+];
